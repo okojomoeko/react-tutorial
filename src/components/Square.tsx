7c0a7acc7e7dac3c4e2e006d6808837cfe18f2b1
okojomoeko <chikoro.size@gmail.com>
2020-08-11 08:42:05 +0000

    Making an Interactive Component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Square(props: any) {
+  const [state, setState] = React.useState("");
+
   return (
-    <button className="square">
-      {props.value}
+    <button className="square" onClick={() => setState('X')}>
+      {state}
     </button>
   );
 }
