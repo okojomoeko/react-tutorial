7ab5f5f7b3ca9a86800375226430984e759a2246
okojomoeko <chikoro.size@gmail.com>
2020-08-11 15:31:17 +0000

    完成

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,24 +6,31 @@ import calculateWinner from '../controller/calculateWinner'
 export default function Game() {
   const [xIsNext, setIsNext] = React.useState(true);
   const [history, setHistory] = React.useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = React.useState(0);
 
   function handleClick(i: number) {
-    const _history = history;
+    const _history = history.slice(0, stepNumber + 1);
     const current = _history[_history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, { squares: squares }]);
+    setHistory([..._history, { squares: squares }]);
     setIsNext(!xIsNext);
+    setStepNumber(_history.length)
 
 
   }
 
+  function jumpTo(step: number) {
+    setStepNumber(step);
+    setIsNext((step % 2) === 0)
+  }
+
 
   const _history = history;
-  const current = _history[_history.length - 1];
+  const current = _history[stepNumber];
   const winner = calculateWinner(current.squares);
   let status;
   if (winner) {
@@ -31,6 +38,17 @@ export default function Game() {
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
+
+  const moves = _history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
@@ -38,7 +56,7 @@ export default function Game() {
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
