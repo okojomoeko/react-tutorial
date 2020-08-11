552c63fbac9a45a0b035931c862382ec2bcc2973
okojomoeko <chikoro.size@gmail.com>
2020-08-11 15:33:07 +0000

    Display the location for each move in the format (col, row) in the move history list.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Board from './Board';
-import calculateWinner from '../controller/calculateWinner'
-
+import React from "react";
+import Board from "./Board";
+import calculateWinner from "../controller/calculateWinner";
 
 export default function Game() {
   const [xIsNext, setIsNext] = React.useState(true);
-  const [history, setHistory] = React.useState([{ squares: Array(9).fill(null) }]);
+  const [history, setHistory] = React.useState([
+    { squares: Array(9).fill(null), moveLoc: { col: 0, row: 0 } },
+  ]);
   const [stepNumber, setStepNumber] = React.useState(0);
 
   function handleClick(i: number) {
@@ -15,34 +16,36 @@ export default function Game() {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = xIsNext ? 'X' : 'O';
-    setHistory([..._history, { squares: squares }]);
+    const col = i % 3;
+    const row = Math.floor(i / 3);
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([
+      ..._history,
+      { squares: squares, moveLoc: { col: col, row: row } },
+    ]);
     setIsNext(!xIsNext);
-    setStepNumber(_history.length)
-
-
+    setStepNumber(_history.length);
   }
 
   function jumpTo(step: number) {
     setStepNumber(step);
-    setIsNext((step % 2) === 0)
+    setIsNext(step % 2 === 0);
   }
-
 
   const _history = history;
   const current = _history[stepNumber];
   const winner = calculateWinner(current.squares);
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = "Winner: " + winner;
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   const moves = _history.map((step, move) => {
-    const desc = move ?
-      'Go to move #' + move :
-      'Go to game start';
+    const desc = move
+      ? `Go to move #${move} (col: ${history[move].moveLoc.col} row: ${history[move].moveLoc.row})`
+      : "Go to game start";
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{desc}</button>
@@ -52,7 +55,12 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={(i: number) => { handleClick(i) }} />
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => {
+            handleClick(i);
+          }}
+        />
       </div>
       <div className="game-info">
         <div>{status}</div>
